dbfacd911a8dd1910492b732a16e0d3bc8c3a6b8
Cole Henderson <cole.hendo.tech@gmail.com>
2023-09-18 18:31:30 -0700

    finished base css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -66,7 +66,7 @@ export class HomeComponent implements AfterViewInit {
     }, 5000);
   }
 
-  statement = `
+  statementStart = `
 I took my first coding class to fulfill a college math credit. I never looked back.
 
 Ever since, that curiosity has led me on a journey through a variety of disciplines, industries, and interests, with a constant goal of growth.
@@ -75,14 +75,14 @@ What has that journey taught me?
 
 I am driven by passion, curiosity, and quality; planning is more important than execution; and empowering colleagues is the greatest value generator over time. It has also helped me realize my greatest strengths: organization, decisiveness, and communication.
 
-Additionally, it has let me showcase my skills through a variety of projects
+Additionally, it has let me showcase my skills through a variety of projects`;
 
-  -  I built and scaled a company-wide ticket automation site, slashing
-     my team’s hiring need in half
-  -  I led multiple cloud cost reduction efforts, reducing total AWS
-     bills by double-digit percentiles in all instances
-  -  I increased applications to my team by 10-fold through rigorous
-     restructuring and reformatting of the underlying hiring process
-
+  statementEnd = `
 I specialize in Full Stack Development and Cloud Computing — specifically Amazon Web Services (AWS). I am proficient in Python, Angular, Bash, JavaScript, and Groovy, as well as the DevOps framework, including Kubernetes, Terraform, Docker, and Linux — two lists I am constantly working to expand.`;
+
+  statementProjectsList = [
+    `I built and scaled a company-wide ticket automation site, slashing my team’s hiring need in half`,
+    `I led multiple cloud cost reduction efforts, reducing total AWS bills by double-digit percentiles in all instances`,
+    `I increased applications to my team by 10-fold through rigorous restructuring and reformatting of the underlying hiring process`,
+  ];
 }
