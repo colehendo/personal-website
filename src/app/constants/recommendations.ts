459b6c1024aeabecf37d344548645955cd9200b3
Cole Henderson <cole.hendo.tech@gmail.com>
2023-09-19 20:58:39 -0700

    fixed css bugs

--- src/app/constants/recommendations.ts
+++ src/app/constants/recommendations.ts
@@ -2,6 +2,19 @@ import { IRecommendation } from '../interfaces/recommendations';
 
 export const RECOMMENDATIONS: IRecommendation[] = [
   {
+    name: 'Jean-Marie Zoda',
+    title: 'Director of Platform Engineering and Technology',
+    company: 'Room 40 / NGDI',
+    relation: 'Coworker at 4G Clinical',
+    linkedin: 'https://www.linkedin.com/in/jean-marie-zoda-b1528623/',
+    recommendation: `
+First and foremost, it is important to understand that 4G Clinical only hires promising, high-potential, and even brilliant people, and I feel privileged to have been a part of this company. As such, it was at 4G Clinical that I had the chance to meet Cole.
+
+Like me, he was part of the DevOps/SRE team. The tasks cannot be listed here, however, it involved mastering the infrastructure, being Cloud certified, knowing our many DevOps tools, ditto for the strategic tools that enabled deployment, but also the lifecycle of our applications, providing developer support, and in some cases 24/7 IT support with respect, empathy, patience, kindness and the list goes on. In fact, Cole has always been available and willing to help, including me. As a team, we didn't count our hours, and many weekends were devoted with courage and pugnacity to the company's projects.
+
+We collaborated together, shared knowledge and exchanged views through many discussions. Cole is talented, sharp, and not only smart but brilliant. I am proud to respond to his request with a letter of recommendation and consider Cole one of the fine and talented people I have had the good fortune to meet at 4G Clinical. His future will no doubt be bright!`,
+  },
+  {
     name: 'Gerard Brown',
     title: 'Senior IT Manager',
     company: '4G Clinical',
@@ -20,18 +33,5 @@ He dug into problems that we wanted to solve and found solutions and practical w
     linkedin: 'https://www.linkedin.com/in/alex-cline-ab047010a/',
     recommendation: `
 Cole was quite beneficial to our DevOps team here at 4G. He was dedicated, hard-working and happy to help however he could. He was good about reaching out when he was stuck and could also take the initiative when given a project to run with. We're very pleased with his work here and I for one would be happy to see him back on any team I was a part of in the future.`,
-  },
-  {
-    name: 'Jean-Marie Zoda',
-    title: 'Director of Platform Engineering and Technology',
-    company: 'Room 40 / NGDI',
-    relation: 'Coworker at 4G Clinical',
-    linkedin: 'https://www.linkedin.com/in/jean-marie-zoda-b1528623/',
-    recommendation: `
-First and foremost, it is important to understand that 4G Clinical only hires promising, high-potential, and even brilliant people, and I feel privileged to have been a part of this company. As such, it was at 4G Clinical that I had the chance to meet Cole.
-
-Like me, he was part of the DevOps/SRE team. The tasks cannot be listed here, however, it involved mastering the infrastructure, being Cloud certified, knowing our many DevOps tools, ditto for the strategic tools that enabled deployment, but also the lifecycle of our applications, providing developer support, and in some cases 24/7 IT support with respect, empathy, patience, kindness and the list goes on. In fact, Cole has always been available and willing to help, including me. As a team, we didn't count our hours, and many weekends were devoted with courage and pugnacity to the company's projects.
-
-We collaborated together, shared knowledge and exchanged views through many discussions. Cole is talented, sharp, and not only smart but brilliant. I am proud to respond to his request with a letter of recommendation and consider Cole one of the fine and talented people I have had the good fortune to meet at 4G Clinical. His future will no doubt be bright!`,
   },
 ];
