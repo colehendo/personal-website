8168ada8cf1d9aa1527cd283045fa7a47d02a398
Cole Henderson <cole.hendo.tech@gmail.com>
2023-11-01 10:35:27 -0700

    updated minor details

--- src/app/data/recommendations.ts
+++ src/app/data/recommendations.ts
@@ -5,7 +5,7 @@ export const RECOMMENDATIONS: IRecommendation[] = [
     name: 'Rob Campanella',
     title: 'VP Technology',
     company: '4G Clinical',
-    relation: 'Manager at 4G Clinical',
+    relation: `Cole's Manager at 4G Clinical`,
     linkedin: 'https://www.linkedin.com/in/rob-campanella-5223a51/',
     recommendation: `
 Cole is basically famous at 4G Clinical. His first summer here, he was "the intern" somewhere in the middle of his time at college. I believe he anticipated a summer of fetching coffee for others but quickly learned we intended to have him actually help move some devops priorities forward. And boy did he help. And learn. He absorbed everything. You didn't need to explain things more than once, and he was off and running. At the end of the summer I told him he would be welcomed back the following summer or even during Thanksgiving/Xmas breaks when he was back from college - if he was interested. To our delight, he was. He helped us for another summer and multiple school vacations before we extended him an offer to join our team full time once he graduated. 
@@ -18,7 +18,7 @@ Cole is absolutely someone I would bring back onto my team if I ever have the ch
     name: 'Kieran Bellew',
     title: 'Software Engineering Manager',
     company: '4G Clinical',
-    relation: 'Coworker at 4G Clinical',
+    relation: `Cole's Coworker at 4G Clinical`,
     linkedin: 'https://www.linkedin.com/in/kieran-bellew/',
     recommendation: `
 Cole is a pleasure to work with. He brings a unique blend of thoughtfulness, compassion, approachability, and an ability to think critically about complex problems. While at 4G, Cole grew to become a vital part of the DevOps team, particularly through his full-stack development work on a crucial platform that enabled every team at the organisation. He continuously pursued difficult projects and training to challenge himself and improve his skills as an engineer. It was sad to see him go, but it's exciting to see what challenge he'll pick next.`,
@@ -27,7 +27,7 @@ Cole is a pleasure to work with. He brings a unique blend of thoughtfulness, com
     name: 'Jean-Marie Zoda',
     title: 'Director of Platform Engineering and Technology',
     company: 'Room 40 / NGDI',
-    relation: 'Coworker at 4G Clinical',
+    relation: `Cole's Coworker at 4G Clinical`,
     linkedin: 'https://www.linkedin.com/in/jean-marie-zoda-b1528623/',
     recommendation: `
 First and foremost, it is important to understand that 4G Clinical only hires promising, high-potential, and even brilliant people, and I feel privileged to have been a part of this company. As such, it was at 4G Clinical that I had the chance to meet Cole.
@@ -40,7 +40,7 @@ We collaborated together, shared knowledge and exchanged views through many disc
     name: 'Gerard Brown',
     title: 'Senior IT Manager',
     company: '4G Clinical',
-    relation: 'Coworker at 4G Clinical',
+    relation: `Cole's Coworker at 4G Clinical`,
     linkedin: 'https://www.linkedin.com/in/gerard-brown-44b-82a-84/',
     recommendation: `
 Cole is a bright, hardworking professional. 
@@ -51,7 +51,7 @@ He dug into problems that we wanted to solve and found solutions and practical w
     name: 'Alex Cline',
     title: 'DevOps Automation Manager',
     company: '4G Clinical',
-    relation: 'Coworker at 4G Clinical',
+    relation: `Cole's Coworker at 4G Clinical`,
     linkedin: 'https://www.linkedin.com/in/alex-cline-ab047010a/',
     recommendation: `
 Cole was quite beneficial to our DevOps team here at 4G. He was dedicated, hard-working and happy to help however he could. He was good about reaching out when he was stuck and could also take the initiative when given a project to run with. We're very pleased with his work here and I for one would be happy to see him back on any team I was a part of in the future.`,
